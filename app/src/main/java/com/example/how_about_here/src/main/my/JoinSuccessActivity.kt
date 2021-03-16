cffbebe5b729d9130fdf9e557b2b5e43023424af
skye3870 <skye3870@naver.com>
2021-03-16 09:59:10 +0900

    210315 commit

--- app/src/main/java/com/example/how_about_here/src/main/my/JoinSuccessActivity.kt
+++ app/src/main/java/com/example/how_about_here/src/main/my/JoinSuccessActivity.kt
@@ -4,22 +4,22 @@ import android.content.Intent
 import android.os.Bundle
 import com.example.how_about_here.R
 import com.example.how_about_here.config.BaseActivity
-import com.example.how_about_here.databinding.ActivityCouponPopupBinding.inflate
 import com.example.how_about_here.databinding.ActivityJoinAgreeBinding
-import com.example.how_about_here.databinding.ActivityJoinFormBinding
 import com.example.how_about_here.databinding.ActivityJoinLoginBinding
+import com.example.how_about_here.databinding.ActivityJoinSuccessBinding
 import com.example.how_about_here.databinding.ActivityMainBinding
 
 
-class JoinFormActivity : BaseActivity<ActivityJoinFormBinding>(ActivityJoinFormBinding::inflate) {
+class JoinSuccessActivity : BaseActivity<ActivityJoinSuccessBinding>(ActivityJoinSuccessBinding::inflate) {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding.agree.setOnClickListener(){
-            val intent = Intent(this, JoinSuccessActivity::class.java)
-            startActivity(intent)
+        binding.check.setOnClickListener(){
+            finish()
         }
+
     }
 
 
